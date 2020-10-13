320da86b328ee8532a84a747f2e7534c9b7443ac
Heinrich Dedekind <heinrich.dedekind@gmail.com>
2020-10-13 08:13:19 +0200

    Initial code base

---
+++ src/com/heinrich24/MyMain.java
@@ -0,0 +1,20 @@
+package com.heinrich24;
+
+import java.util.Scanner;
+
+public class MyMain {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
+
+	}
+}
+
+class MyRegex {
+
+	// Regex pattern that validate IPv4 address.
+	public String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+}
